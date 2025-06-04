cc648f7f25c30327734c44a5fd883a1fba70b65a
Arshpreet singh <arshpreetsingh1327@gmail.com>
2025-06-04 17:34:39 +0530

    aded

--- dashboard/src/app/app.component.ts
+++ dashboard/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './Home/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
